9e50e87478ae2ec337bd621a7c1f1003db76ea88
fishkingsin <fishkingsin@gmail.com>
2013-04-09 01:02:32 +0800

    update shader

--- src/ofxLEDsLPD8806.cpp
+++ src/ofxLEDsLPD8806.cpp
@@ -52,7 +52,7 @@ ofxLEDsLPD8806::ofxLEDsLPD8806(const size_t _numLEDs)
 		<< "	vec2 pos = texCoordVarying;\n"
 		<< "	vec3 src = texture2D(tex0, pos).rgb;\n"
 		<< "	vec3 lpd8806Color = src*0.5 + 0.5+0.004;\n"
-		<< "	gl_FragColor = vec4(lpd8806Color.rgb,1.0);\n"
+		<< "	gl_FragColor = vec4(lpd8806Color.grb,1.0);\n"
 		<< "}\n";
 		//		<< "	gl_FragColor = vec4( src , 1.0);\n"
 		ofLogVerbose("ofxLEDsLPD8806")<< fragmentShaderSource.str();
